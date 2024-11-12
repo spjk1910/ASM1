f6beab5ef86d95b7be73c28bf2e84ed93b11dbcc
spjk1910 <trangdpt1910@gmail.com>
2024-11-12 19:26:57 +0700

    Apply language changing for SettingView and WelcomeScreen

--- app/src/main/java/com/example/asm1/SettingView.java
+++ app/src/main/java/com/example/asm1/SettingView.java
@@ -1,24 +1,157 @@
-package com.example.asm1;
+    package com.example.asm1;
 
-import android.os.Bundle;
+    import android.app.Activity;
+    import android.content.Intent;
+    import android.content.SharedPreferences;
+    import android.content.res.Configuration;
+    import android.content.res.Resources;
+    import android.os.Bundle;
+    import android.util.Log;
+    import android.widget.ImageView;
+    import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+    import androidx.appcompat.app.AppCompatActivity;
 
-public class SettingView extends AppCompatActivity {
+    import java.util.Locale;
+    import java.util.Objects;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_setting_view);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    public class SettingView extends AppCompatActivity {
+        private static final String PREFS_NAME = "setting";
+        private static final String LANGUAGE_KEY = "language_index";
+        private static final String THEME_KEY = "theme_index";
+        private static final String MUSIC_KEY = "music_index";
+
+        private TextView backButton;
+        private TextView languageChoose, themeChoose, musicChoose;
+        private TextView languageDescription, themeDescription, musicDescription;
+
+        private ImageView chevronLeftLanguage, chevronRightLanguage;
+        private ImageView chevronLeftTheme, chevronRightTheme;
+        private ImageView chevronLeftMusic, chevronRightMusic;
+
+        private int languageIndex;
+        private int themeIndex;
+        private int musicIndex;
+
+        private String[] languages, languageDescriptions;
+        private String[] themes, themeDescriptions;
+        private String[] musics, musicDescriptions;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            Objects.requireNonNull(getSupportActionBar()).hide();
+
+            SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+            languageIndex = prefs.getInt(LANGUAGE_KEY, 0);
+            themeIndex = prefs.getInt(THEME_KEY, 0);
+            musicIndex = prefs.getInt(MUSIC_KEY, 0);
+
+            setContentView(R.layout.activity_setting_view);
+
+            backButton = findViewById(R.id.backButton);
+
+            languageChoose = findViewById(R.id.languageChoose);
+            themeChoose = findViewById(R.id.themeChoose);
+            musicChoose = findViewById(R.id.musicChoose);
+            languageDescription = findViewById(R.id.languageDescription);
+            themeDescription = findViewById(R.id.themeDescription);
+            musicDescription = findViewById(R.id.musicDescription);
+
+            chevronLeftLanguage = findViewById(R.id.chevronLeftLanguage);
+            chevronRightLanguage = findViewById(R.id.chevronRightLanguage);
+            chevronLeftTheme = findViewById(R.id.chevronLeftTheme);
+            chevronRightTheme = findViewById(R.id.chevronRightTheme);
+            chevronLeftMusic = findViewById(R.id.chevronLeftMusic);
+            chevronRightMusic = findViewById(R.id.chevronRightMusic);
+
+            languages = getResources().getStringArray(R.array.languageChosen);
+            languageDescriptions = getResources().getStringArray(R.array.languageDescription);
+            themes = getResources().getStringArray(R.array.themeChosen);
+            themeDescriptions = getResources().getStringArray(R.array.themeDescription);
+            musics = getResources().getStringArray(R.array.musicChosen);
+            musicDescriptions = getResources().getStringArray(R.array.musicDescription);
+
+            updateLanguageDisplay();
+            updateThemeDisplay();
+            updateMusicDisplay();
+
+            setupListeners();
+        }
+
+        private void updateMusicDisplay() {
+            musicChoose.setText(musics[musicIndex]);
+            musicDescription.setText(musicDescriptions[musicIndex]);
+        }
+
+        private void updateThemeDisplay() {
+            themeChoose.setText(themes[themeIndex]);
+            themeDescription.setText(themeDescriptions[themeIndex]);
+        }
+
+        private void updateLanguageDisplay() {
+            languageChoose.setText(languages[languageIndex]);
+            languageDescription.setText(languageDescriptions[languageIndex]);
+        }
+
+        private void setupListeners() {
+            backButton.setOnClickListener(v -> finish());
+            chevronLeftLanguage.setOnClickListener(v -> updateLanguage(-1));
+            chevronRightLanguage.setOnClickListener(v -> updateLanguage(1));
+
+            chevronLeftMusic.setOnClickListener(v -> updateMusic(-1));
+            chevronRightMusic.setOnClickListener(v -> updateMusic(1));
+
+            chevronLeftTheme.setOnClickListener(v -> updateTheme(-1));
+            chevronRightTheme.setOnClickListener(v -> updateTheme(1));
+        }
+
+        private void updateTheme(int i) {
+            themeIndex = (themeIndex + i + themes.length) % themes.length;
+            SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+            prefs.edit().putInt(THEME_KEY, themeIndex).apply();
+            updateThemeDisplay();
+        }
+
+        private void updateMusic(int i) {
+            musicIndex = (musicIndex + i + musics.length) % musics.length;
+            SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+            prefs.edit().putInt(MUSIC_KEY, musicIndex).apply();
+            updateMusicDisplay();
+        }
+
+        private void updateLanguage(int i) {
+            languageIndex = (languageIndex + i + languages.length) % languages.length;
+
+            SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+            prefs.edit().putInt(LANGUAGE_KEY, languageIndex).apply();
+            updateLanguageDisplay();
+
+            if (languageIndex == 1) {
+                setLocale("vi");
+            } else {
+                setLocale("en");
+            }
+            Intent resultIntent = new Intent();
+            setResult(Activity.RESULT_OK, resultIntent);
+            finish();
+            startActivity(getIntent());
+        }
+
+        private void setLocale(String languageCode) {
+            Locale locale = new Locale(languageCode);
+            Locale.setDefault(locale);
+            Resources resources = getResources();
+            Configuration configuration = resources.getConfiguration();
+            configuration.setLocale(locale);
+            resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+        }
+
+        public static String getPrefsName() {
+            return PREFS_NAME;
+        }
+
+        public static String getLanguageKey() {
+            return LANGUAGE_KEY;
+        }
     }
-}
--- app/src/main/java/com/example/asm1/WelcomeScreen.java
+++ app/src/main/java/com/example/asm1/WelcomeScreen.java
@@ -1,24 +1,70 @@
 package com.example.asm1;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Locale;
+import java.util.Objects;
 
 public class WelcomeScreen extends AppCompatActivity {
+    private final ActivityResultLauncher<Intent> settingsLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    SharedPreferences prefs = getSharedPreferences(SettingView.getPrefsName(), MODE_PRIVATE);
+                    int languageIndex = prefs.getInt(SettingView.getLanguageKey(), 0);
+                    String languageCode = (languageIndex == 1) ? "vi" : "en";
+                    setLocale(languageCode);
+                    recreate();
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        SharedPreferences prefs = getSharedPreferences(SettingView.getPrefsName(), MODE_PRIVATE);
+        int languageIndex = prefs.getInt(SettingView.getLanguageKey(), 0);
+        String languageCode = (languageIndex == 1) ? "vi" : "en";
+        setLocale(languageCode);
+
         setContentView(R.layout.activity_welcome_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button startButton = findViewById(R.id.startButton);
+        Button settingButton = findViewById(R.id.settingButton);
+        Button quitButton = findViewById(R.id.quitButton);
+
+        settingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent settingIntent = new Intent(WelcomeScreen.this, SettingView.class);
+                settingsLauncher.launch(settingIntent);
+            }
         });
+
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAffinity();
+            }
+        });
+    }
+
+    private void setLocale(String languageCode) {
+        Locale locale = new Locale(languageCode);
+        Locale.setDefault(locale);
+        getResources().getConfiguration().setLocale(locale);
+        getResources().updateConfiguration(getResources().getConfiguration(), getResources().getDisplayMetrics());
     }
 }
